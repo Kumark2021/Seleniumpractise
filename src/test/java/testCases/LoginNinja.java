66a0efdf5e81090bad3b5726733a9b40ff16bd4e
Kumark2021 <kolavennu20@gmail.com>
2023-11-20 19:23:59 +0530

    Second commit

--- src/test/java/testCases/LoginNinja.java
+++ src/test/java/testCases/LoginNinja.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -11,7 +12,7 @@ public class LoginNinja {
 	
 	@Test()
 	
-	public void NinjaLogin() {
+	public void NinjaLogin() throws InterruptedException {
 	WebDriver driver =new ChromeDriver();
 	//driver.manage().implicitlyWait(Duration.ofSeconds(20));
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
@@ -22,6 +23,16 @@ public class LoginNinja {
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
 	driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+	driver.findElement((By.xpath("//*[@id=\"input-email\"]"))).sendKeys("amootoricap9@gmail.com");
+	driver.findElement(By.xpath("//*[@id=\"input-password\"]")).sendKeys("12345");
+	Thread.sleep(20);
+	driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input")).click();
+	
+//	String expectedTitle="My Account";
+	
+	Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+
+	driver.quit();
 	}
 
 
