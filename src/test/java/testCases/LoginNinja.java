08c2c7094b19c22a4e5a1a7a37974884942c4352
Kumark2021 <kolavennu20@gmail.com>
2023-11-20 20:26:16 +0530

    Login module commit

--- src/test/java/testCases/LoginNinja.java
+++ src/test/java/testCases/LoginNinja.java
@@ -1,5 +1,6 @@
 package testCases;
 import java.time.Duration;
+import java.util.Date;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,7 +11,7 @@ import org.testng.annotations.Test;
 
 public class LoginNinja {
 	
-	@Test()
+	@Test(priority=1)
 	
 	public void NinjaLogin() throws InterruptedException {
 	WebDriver driver =new ChromeDriver();
@@ -34,6 +35,118 @@ public class LoginNinja {
 
 	driver.quit();
 	}
+	
+	@Test(priority=2)
+	
+	public void verifyInvalidLogin()
+	{
+		WebDriver driver =new ChromeDriver();
+		//driver.manage().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//span[contains(text(),'My Account')]")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+		driver.findElement((By.xpath("//*[@id=\"input-email\"]"))).sendKeys("amootoricap239"+generateDateStamp()+"@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"input-password\"]")).sendKeys("1245");
+		driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input")).click();
+		
+		
+		String warningmsg=driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+		String expectedWarningMsg="Warning: No match for E-Mail Address and/or Password.";				
+		Assert.assertTrue(warningmsg.contains(expectedWarningMsg), "Expected Message not displayed");
+		driver.quit();
+		
+	}
+	
+	public String generateDateStamp()
+	{
+		Date date=new Date();
+		return date.toString().replace(" ", "_").replace(":", "_");
+		
+				
+	}
+	
+	@Test(priority=3)
+	
+	public void loginwithInvalidEmailandValidpassword()
+	{
+		WebDriver driver =new ChromeDriver();
+		//driver.manage().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//span[contains(text(),'My Account')]")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+		driver.findElement((By.xpath("//*[@id=\"input-email\"]"))).sendKeys("amootoricap239"+generateDateStamp()+"@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"input-password\"]")).sendKeys("12345");
+		driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input")).click();
+		
+		
+		String warningmsg=driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+		String expectedWarningMsg="Warning: No match for E-Mail Address and/or Password.";				
+		Assert.assertTrue(warningmsg.contains(expectedWarningMsg), "Expected Message not displayed");
+		driver.quit();
+		
+	}
+
+	@Test(priority=4)
+	
+	public void loginwithValidemailandInvalidemail()
+	{
+		WebDriver driver =new ChromeDriver();
+		//driver.manage().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//span[contains(text(),'My Account')]")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+		driver.findElement((By.xpath("//*[@id=\"input-email\"]"))).sendKeys("amootoricap9@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"input-password\"]")).sendKeys("1245");
+		driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input")).click();
+		
+		
+		String warningmsg=driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+		String expectedWarningMsg="Warning: No match for E-Mail Address and/or Password.";				
+		Assert.assertTrue(warningmsg.contains(expectedWarningMsg), "Expected Message not displayed");
+		driver.quit();
+		
+		
+		
+	}
+	@Test(priority=5)
+	public void withoutEmailandPassword()
+	{
+		WebDriver driver =new ChromeDriver();
+		//driver.manage().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//span[contains(text(),'My Account')]")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+		driver.findElement((By.xpath("//*[@id=\"input-email\"]"))).sendKeys("");
+		driver.findElement(By.xpath("//*[@id=\"input-password\"]")).sendKeys("");
+		driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input")).click();
+		
+		
+		String warningmsg=driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+		String expectedWarningMsg="Warning: No match for E-Mail Address and/or Password.";				
+		Assert.assertTrue(warningmsg.contains(expectedWarningMsg), "Expected Message not displayed");
+		driver.quit();
+		
+		
+	}
 
 }
